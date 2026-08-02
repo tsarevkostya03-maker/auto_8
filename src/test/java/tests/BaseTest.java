66b6a244b49c0786fcb22378be69333648847bb8
tsarevkostya03-maker <tsarevkostya03@gmail.com>
2026-08-02 22:48:28 +0300

    Update BaseTest.java

--- src/test/java/tests/BaseTest.java
+++ src/test/java/tests/BaseTest.java
@@ -7,11 +7,5 @@ import org.junit.jupiter.api.BeforeAll;
 public class BaseTest {
     @BeforeAll
     static void setUpAll() {
-        WebDriverManager.chromedriver().setup();
-        Configuration.browser = "chrome";
-        Configuration.headless = Boolean.parseBoolean(System.getProperty("selenide.headless", "false"));
-        Configuration.timeout = 10000;
-        Configuration.browserSize = "1920x1080";
-        System.out.println("Chrome driver configured, headless: " + Configuration.headless);
     }
 }
